8ef2189a1a9647ae408f7fd2551cc395fa1dd3ec
Nagehan54 <nagehanaydin@yahoo.com>
2023-04-29 14:10:38 +0300

    first commit

---
+++ todoapp/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState , useEffect, useRef} from 'react'
+import { useTodoLayerValue } from './context/TodoContext'
+import TodoList from './components/TodoList'
+import './App.css';
+
+const App = () => {
+  const [{ todos }, dispatch] = useTodoLayerValue();
+  const [content, setContent] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+      if(!content &&  content.length < 1) return;
+
+      const newTodo = {
+        id: Math.floor(Math.random() * 345454665),
+        content,
+        isCompleted: false
+      };
+
+      dispatch({
+        type: 'ADD_TODO' ,
+        payload: newTodo,
+      });
+
+      setContent ('');
+  };
+
+
+  return (
+    <div className='container'>
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input 
+        type="text" 
+        className='todo-input' 
+        onChange={(event) => setContent(event.target.value)} 
+        value={content} 
+        ref={inputRef}
+        />
+        <button className='todo-button'>
+          Ekle
+        </button>
+
+      </form>
+
+      {/* todo listesi */}
+      <TodoList todos={todos} />
+    </div>
+  )
+}
+
+export default App
